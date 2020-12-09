084752e1af111192568b3548d6a71352ea35ba2d
taesu666 <wmiso.j@gmail.com>
2020-12-09 17:14:51 +0900

    contact 화면 만들기

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,45 @@
-function Contact() {
-  return <div>contact</div>;
+import pluseLogo from "../img/control_point-24px.svg";
+import porfileInit from "../img/account_circle-24px.svg";
+
+function Contact(props) {
+  const {address} = props;
+  return (
+  <>
+    <div id ="nav-wrapper">
+
+      <div id ="title">
+        <p>Contacts</p>
+      </div>
+      <div id = "search">
+        <input type ="text" id = "search-box"></input>
+        <img src={pluseLogo} id="pluse-button" ></img>
+      </div>
+    
+    </div>
+
+    <div id="contacts-wrapper" className="wrapper">
+    
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8,9 ,10 ,11, 12].map((val) => (
+            <div key={val} id="contacts-list">
+              <img
+                className="contacts-init"
+                src={porfileInit}
+                alt="profile"
+                width="30px"
+              />
+              <div id="contacts-info">
+                <p className="name ellip">Anonymous</p>
+                <p className="address ellip">
+                  {address ? address : "can't find address"}
+                </p>
+              </div>
+            </div>
+          ))}
+      </div>
+    
+
+  </>
+  );
 }
 
 export default Contact;
