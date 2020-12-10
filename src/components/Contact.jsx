bf19b8e83966df68a4ee5d8710f849b70714c37d
Dayong Lee <50468628+dayongbz@users.noreply.github.com>
2020-12-10 23:07:10 +0900

    연락처 페이지 스타일 수정 및 a 태그 데코 없앰

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,47 +1,39 @@
-import pluseLogo from "../img/control_point-24px.svg";
+import plusLogo from "../img/control_point-24px.svg";
 import porfileInit from "../img/account_circle-24px.svg";
 
 function Contact(props) {
-  const {address} = props;
+  const { address } = props;
   return (
-  <>
-    <div id ="nav-wrapper">
-
-      <div id ="title">
-        <p>Contacts</p>
+    <>
+      <div id="nav-wrapper">
+        <div id="title">
+          <p>Contacts</p>
+        </div>
+        <div id="search-wrapper">
+          <input type="text" id="search-box"></input>
+          {/* <img src={plusLogo} id="plus-button" alt="plus button" /> */}
+        </div>
       </div>
-      <div id = "search">
-       
-        <input type ="text" id = "search-box"></input>
-        <img src={pluseLogo} id="pluse-button" >
 
-        </img>
-      </div>
-    
-    </div>
-
-    <div id="contacts-wrapper" className="wrapper">
-    
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8,9 ,10 ,11, 12].map((val) => (
-            <div key={val} id="contacts-list">
-              <img
-                className="contacts-init"
-                src={porfileInit}
-                alt="profile"
-                width="30px"
-              />
-              <div id="contacts-info">
-                <p className="name ellip">Anonymous</p>
-                <p className="address ellip">
-                  {address ? address : "can't find address"}
-                </p>
-              </div>
+      <div id="contacts-wrapper" className="wrapper">
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((val) => (
+          <div key={val} id="contacts-list">
+            <img
+              className="contacts-init"
+              src={porfileInit}
+              alt="profile"
+              width="30px"
+            />
+            <div id="contacts-info">
+              <p className="name ellip">Anonymous</p>
+              <p className="address ellip">
+                {address ? address : "can't find address"}
+              </p>
             </div>
-          ))}
+          </div>
+        ))}
       </div>
-    
-
-  </>
+    </>
   );
 }
 
