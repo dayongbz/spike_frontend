3646859d806657c9dc414668884c9cc19b358622
taesu666 <wmiso.j@gmail.com>
2020-12-16 23:06:05 +0900

    main에 record만듬

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,11 @@
 import logo from "../assets/img/logo.png";
 import porfileInit from "../assets/img/account_circle-24px.svg";
 import etherLogoCircle from "../assets/img/ether.png";
-
+import Button from "../components/common/Button";
+import sendIcon from"../assets/img/send.svg";
+import acceptIcon from"../assets/img/accept.svg";
 function Main(props) {
-  const { address, balance } = props;
+  const { address, balance,onClick } = props;
   return (
     <>
       <div id="nav-wrapper">
@@ -32,7 +34,9 @@ function Main(props) {
               </div>
             </div>
             <p className="address ellip">{address}</p>
-            <button>Send</button>
+            <Button rounded="true" onClick={onClick}>
+              Send
+            </Button>
           </div>
 
         </div>
@@ -56,6 +60,9 @@ function Main(props) {
                   {address ? address : "can't find address"}
                 </p>
               </div>
+              <button id ="constacts-button">
+                <p id="constacts-button-text">send</p>
+                </button>
             </div>
           ))}
         </div>
@@ -65,45 +72,50 @@ function Main(props) {
         <p className="title">Record</p>
 
         <div id="record-slider" className="slider scrollbar">
-          <div id="send-record" className="record">
-            <img
-                className="send-init"
-                src={porfileInit}
-                alt="profile"
-                width="30px"
-              />
-              <div id="send-info">
-                <p className="name ellip">이다용</p>
+          {[0,1,2,3,4,5,6,7,8,9,10,11,12].map((val) =>(
+            <div key={val} id ="record-list">
+              <div id="send-record" className="record">
+              <img
+                  className="send-init"
+                  src={sendIcon}
+                  alt="profile"
+                  width="16px"
+                />
+                <div id="send-info">
+                  <p className="name ellip">이다용</p>
+                </div>
+                <div id= "send-balanceInfo" className="balanceInfo">
+                  <p id= "send-balance" className="balance">
+                    <span className="type">ETH</span>
+                    {balance}
+                  </p>
+                  <p id= "send-dollar" className="dollar">${balance * 575}</p>
+                </div>
+            </div>
+              <div id="accept-record" className="record">
+                <img
+                    className="accept-init"
+                    src={acceptIcon}
+                    alt="profile"
+                    width="16px"
+                  />
+                  <div id="accept-info">
+                  <p className="address ellip">
+                      {address ? address : "can't find address"}
+                    </p>
+                  </div>
+                  <div id= "accept-balanceInfo" className="balanceInfo">
+                    <p id= "accept-balance" className="balance">
+                      <span className="type">ETH</span>
+                      {balance}
+                    </p>
+                    <p id= "accept-dollar" className="dollar">${balance * 575}</p>
+                  </div>
               </div>
-              <div id= "send-balanceInfo" className="balanceInfo">
-                <p id= "send-balance" className="balance">
-                  <span className="type">ETH</span>
-                  {balance}
-                </p>
-                <p id= "send-dollar" className="dollar">${balance * 575}</p>
-              </div>
-          </div>
-
-          <div id="accept-record" className="record">
-            <img
-                className="accept-init"
-                src={porfileInit}
-                alt="profile"
-                width="30px"
-              />
-              <div id="accept-info">
-              <p className="address ellip">
-                  {address ? address : "can't find address"}
-                </p>
-              </div>
-              <div id= "accept-balanceInfo" className="balanceInfo">
-                <p id= "accept-balance" className="balance">
-                  <span className="type">ETH</span>
-                  {balance}
-                </p>
-                <p id= "accept-dollar" className="dollar">${balance * 575}</p>
-              </div>
-          </div>
+            </div>
+          ))}
+         
+        
         </div>
       </div>
     </>
