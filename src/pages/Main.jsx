61124a982428e3c3f16256b693415ed9643261d1
taesu666 <wmiso.j@gmail.com>
2020-12-16 22:30:00 +0900

    no message

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -65,22 +65,45 @@ function Main(props) {
         <p className="title">Record</p>
 
         <div id="record-slider" className="slider scrollbar">
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((val) => (
-            <div key={val} id="contacts-list">
-              <img
-                className="contacts-init"
+          <div id="send-record" className="record">
+            <img
+                className="send-init"
                 src={porfileInit}
                 alt="profile"
                 width="30px"
               />
-              <div id="contacts-info">
-                <p className="name ellip">Anonymous</p>
-                <p className="address ellip">
+              <div id="send-info">
+                <p className="name ellip">이다용</p>
+              </div>
+              <div id= "send-balanceInfo" className="balanceInfo">
+                <p id= "send-balance" className="balance">
+                  <span className="type">ETH</span>
+                  {balance}
+                </p>
+                <p id= "send-dollar" className="dollar">${balance * 575}</p>
+              </div>
+          </div>
+
+          <div id="accept-record" className="record">
+            <img
+                className="accept-init"
+                src={porfileInit}
+                alt="profile"
+                width="30px"
+              />
+              <div id="accept-info">
+              <p className="address ellip">
                   {address ? address : "can't find address"}
                 </p>
               </div>
-            </div>
-          ))}
+              <div id= "accept-balanceInfo" className="balanceInfo">
+                <p id= "accept-balance" className="balance">
+                  <span className="type">ETH</span>
+                  {balance}
+                </p>
+                <p id= "accept-dollar" className="dollar">${balance * 575}</p>
+              </div>
+          </div>
         </div>
       </div>
     </>
